bab01d16ca69c0b3e574618821348a0197d9c7fc
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-03-19 10:29:16 +0100

    navlink ist för link = markerar vilken sida som är aktuell! :)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function NavBar() {
     return (
         <header>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/projects">Projects</NavLink></li>
                 </ul>
             </nav>
         </header>
