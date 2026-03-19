416aeee324c299a7aec1ea68fcf95e017081a5c9
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-03-19 09:53:25 +0100

    BrowserRouter and routes to specific paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Projects from './pages/projects'
+
+
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
