2cb11f8be6dd062a1eefdaf510e76691d6f7dd80
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-03-19 10:40:18 +0100

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Projects from './pages/projects'
-
 import NavBar from './components/NavBar'
-
-
 
 function App() {
 
